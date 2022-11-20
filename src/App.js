90ed911710bc26f69e3f0291685d9c0f8c03e82f
Greg Doud <gpdoud@gmail.com>
2022-11-20 14:28:59 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const uri = "http://localhost:5000/api/users/1";
+
+const callUserGet = async () => {
+  const res = await fetch(uri);
+  const user = await res.json();
+  console.debug("User:", user);
+  return user;
+}
 
 function App() {
+  const [ user, setUser ] = useState({});
+
+  useEffect(() => {
+    callUserGet()
+      .then(res => setUser(res))
+    },[]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className='table table-sm'>
+      <tbody>
+        <tr><td>Id</td><td>{user.id}</td></tr>
+        <tr><td>Username</td><td>{user.username}</td></tr>
+        <tr><td>Firstname</td><td>{user.firstname}</td></tr>
+        <tr><td>Lastname</td><td>{user.lastname}</td></tr>
+        <tr><td>Phone</td><td>{user.phone}</td></tr>
+        <tr><td>Email</td><td>{user.email}</td></tr>
+        <tr><td>Reviewer</td><td>{user.isReviewer ? "Yes" : "No"}</td></tr>
+        <tr><td>Admin</td><td>{user.isAdmin ? "Yes" : "No"}</td></tr>
+      </tbody>
+    </table>
   );
 }
 
